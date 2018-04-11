80d1e402b5e80d4b2c2feded92682ff6a19e297f
Justin Leonard <jhuskies@hotmail.com>
2018-04-11 16:43:08 -0700

    Completed exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -11,44 +11,47 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data frame
 # You can use `View()` to inspect it
-
+View(vehicles)
 
 # Select the different manufacturers (makes) of the cars in this data set. 
 # Save this vector in a variable
-
+makes <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set
-
+unique(makes)
 
 # Filter the data set for vehicles manufactured in 1997
-
+vehicles_1997 <- filter(vehicles, year == 1997)
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function to get a vector of indices in order by value
 # See also:
 # https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
+vehicles_1997[order(vehicles_1997$hwy), ]
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average
 # gas milage (between city and highway mpg) for each car
-
+vehicles_1997$average <- (vehicles_1997$cty + vehicles_1997$hwy) / 2
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more
 # than 20 miles/gallon in the city. 
 # Save this new data frame in a variable.
-
+city_cars <- filter(vehicles, vehicles$drive == "2-Wheel Drive", vehicles$cty > 20)
 
 # Of the above vehicles, what is the vehicle ID of the vehicle with the worst 
 # hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+select(filter(city_cars, city_cars$hwy == min(city_cars$hwy)), "id")
 
 # Write a function that takes a `year_choice` and a `make_choice` as parameters, 
 # and returns the vehicle model that gets the most hwy miles/gallon of vehicles 
 # of that make in that year.
 # You'll need to filter more (and do some selecting)!
-
+efficient <- function(year_choice, make_choice) {
+  filtered_cars <- filter(vehicles, vehicles$make == make_choice, vehicles$year == year_choice)
+  select(filter(filtered_cars, filtered_cars$hwy == max(filtered_cars$hwy)), "model")
+}
 
 # What was the most efficient Honda model of 1995?
-
+efficient(1995, "Honda")
